6c10322ddef6befe12d2f22f68c55c081bbd531e
hukappa <54602340+Shidoengie@users.noreply.github.com>
2023-02-16 14:58:33 +0000

    making devspeed a easteregg

--- screens/Options/Options.gd
+++ screens/Options/Options.gd
@@ -1,5 +1,7 @@
 extends Control
+onready var devspeed_check_box = $"%DevspeedCheckBox"
 
+var dev_unlock = 0
 func _on_DevspeedCheckBox_toggled(button_pressed):
 	Global.devspeed = button_pressed
 func _on_ReturnButton_pressed():
@@ -8,3 +10,11 @@ func _on_ReturnButton_pressed():
 
 func _on_Control_ready():
 	$Panel/VBoxContainer2/Devspeed/DevspeedCheckBox.pressed = Global.devspeed
+
+
+func _on_SecretButton_pressed():
+	if dev_unlock < 7:
+		dev_unlock += 1
+	else:
+		devspeed_check_box.visible = true
+	pass # Replace with function body.
